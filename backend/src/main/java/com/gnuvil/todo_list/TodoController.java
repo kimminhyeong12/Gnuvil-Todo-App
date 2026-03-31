a0ea41cd535bda0a2b04cf19db36097cc9936c9b
kimminhyeong <minhyeong63@gmail.com>
2026-03-31 17:34:46 +0900

    allowed port

--- backend/src/main/java/com/gnuvil/todo_list/TodoController.java
+++ backend/src/main/java/com/gnuvil/todo_list/TodoController.java
@@ -11,6 +11,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/todos")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:5173")
 public class TodoController {
 
     private final TodoService todoService;
