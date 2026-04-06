e7e784ed8439426ef26567b8aa07e9b4dd3c2650
kimminhyeong <minhyeong63@gmail.com>
2026-04-06 14:58:00 +0900

    SecurityConfig 추가

--- backend/src/main/java/com/gnuvil/todo_list/config/SecurityConfig.java
+++ backend/src/main/java/com/gnuvil/todo_list/config/SecurityConfig.java
@@ -3,8 +3,13 @@ package com.gnuvil.todo_list.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.List;
 
 @Configuration
 public class SecurityConfig {
@@ -12,5 +17,22 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable()) // 테스트를 위해 CSRF 비활성화
+                .cors(cors -> cors.configurationSource(request -> {
+                    var config = new CorsConfiguration();
+                    config.setAllowedOrigins(List.of("http://localhost:5173"));
+                    config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+                    config.setAllowedHeaders(List.of("*"));
+                    return config;
+                }))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/**").permitAll() // 해당 경로 허용
+                        .anyRequest().authenticated()
+                );
+        return http.build();
     }
 }
