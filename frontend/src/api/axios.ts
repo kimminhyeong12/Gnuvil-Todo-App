3b5ab316e6a18586fc46bce58780848085d42ec9
kimminhyeong <minhyeong63@gmail.com>
2026-04-30 18:00:14 +0900

    TodoController 변경/ 데이터베이스 postgreSQL로 변경/timeout 추가

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const instance = axios.create({
   baseURL: `${import.meta.env.VITE_API_URL ?? "http://localhost:8080"}/api`,
-  timeout: 5000,
+  timeout: 30000,
 });
 
 instance.interceptors.request.use((config) => {
