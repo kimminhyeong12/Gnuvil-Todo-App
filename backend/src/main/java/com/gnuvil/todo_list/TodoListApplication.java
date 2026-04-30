0a50eee94ca6e529b12da09fa1e98c529422ad85
kimminhyeong <minhyeong63@gmail.com>
2026-04-30 11:40:42 +0900

    TodoController 변경/ 데이터베이스 postgreSQL로 변경

--- backend/src/main/java/com/gnuvil/todo_list/TodoListApplication.java
+++ backend/src/main/java/com/gnuvil/todo_list/TodoListApplication.java
@@ -1,7 +1,9 @@
 package com.gnuvil.todo_list;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TodoListApplication {
@@ -9,5 +11,15 @@ public class TodoListApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TodoListApplication.class, args);
 	}
+
+	@Bean
+	CommandLineRunner checkEnv() {
+		return args -> {
+			System.out.println("DATABASE_URL = " + System.getenv("DATABASE_URL"));
+			System.out.println("DATABASE_USERNAME = " + System.getenv("DATABASE_USERNAME"));
+			System.out.println("DATABASE_DRIVER = " + System.getenv("DATABASE_DRIVER"));
+		};
+	}
+
 
 }
